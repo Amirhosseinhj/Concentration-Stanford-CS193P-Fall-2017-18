51d1bd9e1658f838ea743a51c2e22ab86f134616
Amirhossein Haji Jafari <amirhjhossein@gmail.com>
2018-09-24 21:40:35 +0330

    1. Introduction to iOS 11, Xcode 9 and Swift 4. complete 1:15:00

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,18 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        let cardNumber = cardButtons.index(of: sender)
+        print("cardNumber = \(cardNumber)")
     }
 
     func flipCard(withEmoji emoji: String, on button: UIButton) {
-        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
     }
 
 }
