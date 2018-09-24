320d918fc2292e50a322b9beddeebdff9d16f1fb
Amirhossein Haji Jafari <amirhjhossein@gmail.com>
2018-09-24 20:12:18 +0330

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Amirhossein on 9/24/18.
+//  Copyright © 2018 Amirhossein Haji Jafari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
