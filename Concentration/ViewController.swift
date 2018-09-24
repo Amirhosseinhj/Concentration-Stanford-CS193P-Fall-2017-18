2dbc14a32f94c2e5f9083aa4d71f46f26037f17f
Amirhossein Haji Jafari <amirhjhossein@gmail.com>
2018-09-24 21:48:36 +0330

    Introduction to iOS 11, Xcode 9 and Swift 4. complete

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -19,10 +19,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var flipCountLabel: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
     
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        let cardNumber = cardButtons.index(of: sender)
-        print("cardNumber = \(cardNumber)")
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
     }
 
     func flipCard(withEmoji emoji: String, on button: UIButton) {
