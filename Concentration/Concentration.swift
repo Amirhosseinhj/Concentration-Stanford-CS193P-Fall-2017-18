8b12296dc6636a8d6f960579eec285a5f56abdd9
Amirhossein Haji Jafari <amirhjhossein@gmail.com>
2018-10-01 20:20:39 +0330

    4. More Swift, change concentration class to struct

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Concentration {
+struct Concentration {
     
     private(set) var cards = [Card]()
     
@@ -40,7 +40,7 @@ class Concentration {
     
     var flipCount = 0
     
-    func chooseCard (at index: Int) {
+    mutating func chooseCard (at index: Int) {
         assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
         if !cards[index].isMatched {
             flipCount += 1
